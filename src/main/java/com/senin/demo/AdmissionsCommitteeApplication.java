b02500dc0f6f435c23c9da8c9f2f60af03f1fc3c
Sienin <senin.yura99@gmail.com>
2021-04-20 14:00:42 +0300

    created project

---
+++ src/main/java/com/senin/demo/AdmissionsCommitteeApplication.java
@@ -0,0 +1,13 @@
+package com.senin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmissionsCommitteeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdmissionsCommitteeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/senin/demo/AdmissionsCommitteeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senin.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmissionsCommitteeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
